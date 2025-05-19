ae0a1909fdd6c6c08cf51396df5cc338d18d9ef2
Misho Piradashvili <piradashvili.misho@gmail.com>
2025-05-19 17:03:48 +0400

    Fix: Resolved image display issue due to invalid symlink and added user_id column to images table

--- backend/database/migrations/2025_05_13_122833_add_user_id_to_images_table.php
+++ backend/database/migrations/2025_05_13_122833_add_user_id_to_images_table.php
@@ -4,19 +4,27 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-    public function up(): void
-    {
-        Schema::table('images', function (Blueprint $table) {
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-        });
-    }
+return new class extends Migration
+{
+/**
+* Run the migrations.
+*/
+public function up(): void
+{
+Schema::table('images', function (Blueprint $table) {
+$table->foreignId('user_id')->default(1)->constrained()->onDelete('cascade');
+$table->index('user_id');
+});
+}
 
-    public function down(): void
-    {
-        Schema::table('images', function (Blueprint $table) {
-            $table->dropForeign(['user_id']);
-            $table->dropColumn('user_id');
-        });
-    }
+/**
+* Reverse the migrations.
+*/
+public function down(): void
+{
+Schema::table('images', function (Blueprint $table) {
+$table->dropForeign(['user_id']);
+$table->dropColumn('user_id');
+});
+}
 };
